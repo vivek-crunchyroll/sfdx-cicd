b561514dba88c7f70dfce97447ac0fabb645bc43
Vivek Chander <176272211+vivek-crunchyroll@users.noreply.github.com>
2025-02-03 22:12:53 +0530

    Remove unused Visualforce pages, Aura events, LWC components, and associated metadata files

--- force-app/main/default/lwc/crunchyRollHeader/crunchyRollHeader.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-import crunchLogo from '@salesforce/resourceUrl/CrunchyrollLogo';
-
-export default class CrunchyRollHeader extends LightningElement {
-imageUrl = crunchLogo;
-}
